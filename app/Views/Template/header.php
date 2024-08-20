dae7f4f0b5bb461c923ffcac697086aba19e19c5
ugzer-13 <hugomarcou@gmail.com>
2024-08-20 13:46:17 +0200

    1

--- app/Views/Template/header.php
+++ app/Views/Template/header.php
@@ -5,12 +5,13 @@
     <img src="<?= base_url('images/logo.png') ?>" alt="LOGO">
 </div>
 <div id="sous-logo">
+    <div id="left"></div>
     <div id="recherche">
         <input type="text" name="recherche" id="recherche-inp">
     </div>
     <div id="mini-menu">
         <a href="#"><span class="icones-mini-menu"><i class="fa-solid fa-user"></i></span><br>Mon compte</a>
-        <a href="#">Panier</a>
-        <a href="#">Favoris</a>
+        <a href="#"><span class="icones-mini-menu"><i class="fa-solid fa-user"></i></span><br>Panier</a>
+        <a href="#"><span class="icones-mini-menu"><i class="fa-solid fa-user"></i></span><br>Favoris</a>
     </div>
 </div>
