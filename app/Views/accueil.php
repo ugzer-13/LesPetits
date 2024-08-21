ebec828d873f13b073190879530b490cdef99745
Bdhouha <dou.ha24@gmail.com>
2024-08-21 14:37:29 +0200

    j'ai remplis le footer tel que figma et la je travaille sur la page d'accueil et j'ai commencer a faire des section pour mettre les promotions

--- app/Views/accueil.php
+++ app/Views/accueil.php
@@ -4,4 +4,14 @@
 <?= $this->endSection() ?>
 <?= $this->section('custombody') ?>
 <div>HELLO WORLD</div>
+<section>
+<div class="centre">
+        <div class="bloc-fixe">
+            <title>PROMOTIONS</title>
+            <img src="" alt="">
+        </div>
+        <div class="bloc-fixe">
+            Texte du bloc fixe. Lorem ipsum et compagnie.
+        </div>
+</section>
 <?= $this->endSection() ?>
