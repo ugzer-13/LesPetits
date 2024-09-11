7d43c0f8bdfba875d799fc39cf71867156087031
ugzer-13 <hugomarcou@gmail.com>
2024-09-11 12:36:04 +0200

    footer joli ?

--- app/Views/Template/footer.php
+++ app/Views/Template/footer.php
@@ -1,39 +1,50 @@
-<footer>
-<div id="logo-top">
-    <img src="<?= base_url('images/footer.png') ?>" alt="">
-</div>
-<div>Catégories
-<ul>
-<li>Jeux de société</li>
-<li>Jeux d'extérieur</li>
-<li>Jeux Sportifs</li>
-<li>Jeux de construction</li>
-<li>Jeux de société</li>
-<li>Jeux électroniques</li>
-<li>Activités et jeux d'évei</li>
-</ul></div>
-<div>Marques
-    <ul>
-    
-<li>LEGO</li>
-<li>Playmobil</li>
-<li>V-Tech</li>
+<footer class="py-4">
 
-<li>Funko </li>
-    </ul>
-</div>
-<div>
-    <ul>
-        <li>Qui sommes-nous ?</li>
-<li>Aide
-<ul>
-    <li>Contact</li>
-<li>FAQ</li>
-<li>Mentions légales</li></ul></li>
-<li>Presse</li></ul></div>
-<div class="footer-socials">
-        <div><i class="fa-brands fa-instagram"></i></div>
-    <div><i class="fa-brands fa-facebook"></i></div>
-    <div><i class="fa-brands fa-twitter"></i></div>
-</div>
+    <div class="container">
+        <div class="row">
+
+            <div id="logo-top">
+                <img src="<?= base_url('images/footer.png') ?>" alt="">
+            </div>
+            <div>Catégories
+                <ul>
+                    <li>Jeux de société</li>
+                    <li>Jeux d'extérieur</li>
+                    <li>Jeux Sportifs</li>
+                    <li>Jeux de construction</li>
+                    <li>Jeux de société</li>
+                    <li>Jeux électroniques</li>
+                    <li>Activités et jeux d'évei</li>
+                </ul>
+            </div>
+            <div>Marques
+                <ul>
+
+                    <li>LEGO</li>
+                    <li>Playmobil</li>
+                    <li>V-Tech</li>
+
+                    <li>Funko </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Qui sommes-nous ?</li>
+                    <li>Aide
+                        <ul>
+                            <li>Contact</li>
+                            <li>FAQ</li>
+                            <li>Mentions légales</li>
+                        </ul>
+                    </li>
+                    <li>Presse</li>
+                </ul>
+            </div>
+            <div class="footer-socials">
+                <div><i class="fa-brands fa-instagram"></i></div>
+                <div><i class="fa-brands fa-facebook"></i></div>
+                <div><i class="fa-brands fa-twitter"></i></div>
+            </div>
+        </div>
+    </div>
 </footer>
